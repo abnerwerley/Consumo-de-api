0983d1f1b13288e21130dafdcc983facc9e159ff
abnerwerley <abnerwerley77@gmail.com>
2022-01-06 23:16:49 -0300

    @abnerwerley[Task: Basic Spring enrironment configured]

---
+++ src/main/java/com/kitsu/KitsuApplication.java
@@ -0,0 +1,13 @@
+package com.kitsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitsuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitsuApplication.class, args);
+	}
+
+}
