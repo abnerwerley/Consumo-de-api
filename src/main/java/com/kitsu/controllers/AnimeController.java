cb118d58ede405a4cf71bd97d97e90f785f35540
abnerwerley <abnerwerley77@gmail.com>
2022-01-17 13:53:36 -0300

    @abnerwerley[Task: Controllers cleaned]

--- src/main/java/com/kitsu/controllers/AnimeController.java
+++ src/main/java/com/kitsu/controllers/AnimeController.java
@@ -1,7 +1,6 @@
 package com.kitsu.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,83 +24,28 @@ public class AnimeController {
 	
 	@GetMapping("/busca/{id}")
 	public Attributes getById(@PathVariable String id) {
-
-		System.out.println("Chegou aqui");
-		String urlFinal = stringBuilder.append(urlApi).append("/anime").append("/").append(id).toString();
-
-		System.out.println(urlFinal);
-		ResponseEntity<Attributes> entity = restTemplate.getForEntity(urlFinal, Attributes.class);
-		
-		Attributes body = entity.getBody();
-		System.out.println(body);
-		
-		//HttpStatus statusCode = entity.getStatusCode();
-		//System.out.println(statusCode);
-		
-		return body;
+		return null;
 	}
 
 	@GetMapping("/listarTodos")
 	public Anime[] pegarTodos(){
-
-		System.out.println("Entrou aqui");
-		RestTemplate restTemplate = new RestTemplate();
-
-		ResponseEntity<Anime[]> response = restTemplate.getForEntity("https://kitsu.io/api/edge/anime", Anime[].class);
-		Anime[] animes = response.getBody();
-
-		System.out.println(animes);
-		return animes;
+		return null;
 	}
 	
 	@GetMapping("/busca/{texto}")
 	public Attributes getAnimePorTexto(@PathVariable String texto) {
-
-		System.out.println("Chegou aqui");
-		String urlFinal = stringBuilder.append(urlApi).append("/anime").append("?filter[text]=").append(texto).toString();
-
-		System.out.println(urlFinal);
-		ResponseEntity<Attributes> entity = restTemplate.getForEntity(urlFinal, Attributes.class);
-		
-		Attributes body = entity.getBody();
-		System.out.println(body);
-		
-		//HttpStatus statusCode = entity.getStatusCode();
-		//System.out.println(statusCode);
-		
-		return body;
+		return null;
 	}
 	
 	@GetMapping("/trending")
 	public Anime[] TrendingAnimes(){
-
-		System.out.println("Entrou aqui");
-		RestTemplate restTemplate = new RestTemplate();
-
-		ResponseEntity<Anime[]> response = restTemplate.getForEntity("https://kitsu.io/api/edge/trending/anime", Anime[].class);
-		Anime[] TrendingAnimes = response.getBody();
-
-		System.out.println(TrendingAnimes);
-		return TrendingAnimes;
+		return null;
 	}
 	
 	
 	@GetMapping("/episode/{anime}")
 	public Episodes getEpisodePorAnime(@PathVariable String anime) {
-
-		System.out.println("Chegou aqui");
-		String urlFinal = stringBuilder.append(urlApi).append("/anime").append("?filter[text]=").append(anime).toString();
-
-		System.out.println(urlFinal);
-		ResponseEntity<Episodes> entity = restTemplate.getForEntity(urlFinal, Episodes.class);
-		
-		Episodes body = entity.getBody();
-		System.out.println(body);
-		
-		//HttpStatus statusCode = entity.getStatusCode();
-		//System.out.println(statusCode);
-		
-		return body;
+		return null;
 	}
 
 }
--- src/main/java/com/kitsu/controllers/MangaController.java
+++ src/main/java/com/kitsu/controllers/MangaController.java
@@ -1,16 +1,14 @@
 package com.kitsu.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.kitsu.models.Anime;
-import com.kitsu.models.Anime.Attributes;
-import com.kitsu.models.Anime.Episodes;
+import com.kitsu.models.Manga.Attributes;
+import com.kitsu.models.Manga.Chapters;
 import com.kitsu.models.Manga;
 
 @RestController
@@ -25,72 +23,27 @@ public class MangaController {
 
 	@GetMapping("/busca/{id}")
 	public Attributes getById(@PathVariable String id) {
-
-		System.out.println("Chegou aqui");
-		String urlFinal = stringBuilder.append(urlApi).append("/manga").append("/").append(id).toString();
-		System.out.println(urlFinal);
-
-		ResponseEntity<Attributes> entity = restTemplate.getForEntity(urlFinal, Attributes.class);
-
-		Attributes body = entity.getBody();
-		return body;
+		return null;
 	}
 
 	@GetMapping("/listarTodos")
 	public Manga[] getAll() {
-
-		System.out.println("Entrou aqui");
-		RestTemplate restTemplate = new RestTemplate();
-
-		ResponseEntity<Manga[]> response = restTemplate.getForEntity("https://kitsu.io/api/edge/manga", Manga[].class);
-		Manga[] mangas = response.getBody();
-
-		System.out.println(mangas);
-		return mangas;
+		return null;
 	}
 
 	@GetMapping("/busca/{texto}")
 	public Attributes getMangaPorTexto(@PathVariable String texto) {
-
-		System.out.println("Chegou aqui");
-		String urlFinal = stringBuilder.append(urlApi).append("/manga").append("?filter[text]=").append(texto)
-				.toString();
-
-		System.out.println(urlFinal);
-		ResponseEntity<Attributes> entity = restTemplate.getForEntity(urlFinal, Attributes.class);
-
-		Attributes body = entity.getBody();
-		System.out.println(body);
-		return body;
+		return null;
 	}
 
 	@GetMapping("/trending")
 	public Manga[] TrendingAnimes() {
-
-		System.out.println("Entrou aqui");
-		RestTemplate restTemplate = new RestTemplate();
-
-		ResponseEntity<Manga[]> response = restTemplate.getForEntity("https://kitsu.io/api/edge/trending/manga",
-				Manga[].class);
-		Manga[] TrendingMangas = response.getBody();
-
-		System.out.println(TrendingMangas);
-		return TrendingMangas;
+		return null;
 	}
 
 	@GetMapping("/chapter/{manga}")
-	public Episodes getEpisodePorAnime(@PathVariable String anime) {
-
-		System.out.println("Chegou aqui");
-		String urlFinal = stringBuilder.append(urlApi).append("/manga").append("?filter[text]=").append(anime)
-				.toString();
-
-		System.out.println(urlFinal);
-		ResponseEntity<Episodes> entity = restTemplate.getForEntity(urlFinal, Episodes.class);
-
-		Episodes body = entity.getBody();
-		System.out.println(body);
-		return body;
+	public Chapters getEpisodePorAnime(@PathVariable String manga) {
+		return null;
 	}
 
 }
